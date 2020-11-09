17a12861610a7e0ee131a971852f75e040ca4cf6
Hubnotch <love2ekene@gmail.com>
2020-11-09 07:25:24 +0100

    coded the styls and entry point of the script

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+
+let generate = document.getElementById("generate");
+let numberHolder = document.querySelector(".number_holder");
+
+numberHolder.addEventListener('click', updateDom)
+
+console.log(generate, numberHolder);
+
+function updateDom() {
+
+
+}
+
+
